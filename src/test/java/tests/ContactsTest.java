84688fc3ee9d4d765fc06f618a6419d62f30f604
Azhati Yelizhate <elzatmozat@gmail.com>
2019-08-13 21:44:26 -0400

    Added contacts tests 2

--- src/test/java/tests/ContactsTest.java
+++ src/test/java/tests/ContactsTest.java
@@ -7,6 +7,7 @@ public class ContactsTest {
 
     @Test
     public void test1(){
-
+        System.out.println("driver opens vytrach");
+        System.out.println("driver goes to the contacts page");
     }
 }
